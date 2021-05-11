561b78519b08c5959358f8dfdef2cc3b95c6e8cb
Ayaka Neko <neko@ayaka.moe>
2021-05-11 18:05:22 +0800

    :sparkles: 采用自动挂载的模式
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- main.go
+++ main.go
@@ -4,11 +4,21 @@ import (
 	"github.com/nekomeowww/pero/config"
 	"github.com/nekomeowww/pero/service"
 	"github.com/nekomeowww/pero/telegram"
+	"github.com/nekomeowww/pero/telegram/controllers/base"
+	"github.com/nekomeowww/pero/telegram/controllers/rss"
+	"github.com/nekomeowww/pero/telegram/dispatcher"
+	"github.com/nekomeowww/pero/telegram/handler"
 )
 
 func main() {
 	conf := config.ParseConfig()
 	// rss.Feed(conf)
 	service.Init(conf)
-	telegram.NewTelegramBot(conf)
+
+	handlers := handler.Groups{
+		base.Handlers(),
+		rss.Handlers(),
+	}
+
+	dispatcher.Dispatch(handlers, telegram.NewTelegramBot(conf))
 }
